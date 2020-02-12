413b808782de608e192e5219f986a901ea3a2e28
Josef Kubin <m4unix@gmail.com>
2020-02-12 15:30:42 +0100

    generated files

--- html_cs.mk
+++ html_cs.mk
@@ -6,22 +6,22 @@ FOLDERS_cs = \
 generovani-kodu-v-m4-uvod \
 generovani-kodu-v-m4-zaklady \
 
-PREVIEW_cs  = $(FOLDERS_cs:=/preview.html)
-VALIDATE_cs = $(FOLDERS_cs:=/validate.html)
-PUBLISH_cs  = $(FOLDERS_cs:=/publish.txt)
-SPCHECK_cs  = $(FOLDERS_cs:=/spell.txt)
+ARTICLE_cs = $(FOLDERS_cs:=/index.html)
+PREVIEW_cs = $(FOLDERS_cs:=/preview.html)
+PUBLISH_cs = $(FOLDERS_cs:=/publish.txt)
+SPCHECK_cs = $(FOLDERS_cs:=/spell.txt)
+ARTICLE  += $(FOLDERS_cs) $(ARTICLE_cs)
 FOLDERS  += $(FOLDERS_cs)
 PREVIEW  += $(FOLDERS_cs) $(PREVIEW_cs)
-VALIDATE += $(FOLDERS_cs) $(VALIDATE_cs)
 PUBLISH  += $(FOLDERS_cs) $(PUBLISH_cs)
 SPCHECK  += $(FOLDERS_cs) $(SPCHECK_cs)
-TARGETS  += $(FOLDERS_cs) $(PREVIEW_cs) $(VALIDATE_cs) $(PUBLISH_cs) $(SPCHECK_cs)
+TARGETS  += $(FOLDERS_cs) $(PREVIEW_cs) $(ARTICLE_cs) $(PUBLISH_cs) $(SPCHECK_cs)
 
 #:html-sub-targets/sub/su	creates all files from generated rules
 .PHONY: html-sub-targets sub su
 html-sub-targets sub su: $(TARGETS)
 
-#:preview/pre/pr/p	for off-line article development
+#:preview/pre/pr/p	as close as possible in real website
 .PHONY: preview pre pr p
 preview pre pr p: $(PREVIEW)
 
@@ -33,13 +33,13 @@ spell sp: $(SPCHECK)
 .PHONY: publish pub pu
 publish pub pu: $(PUBLISH)
 
-#:validate/val/va/v	creates files for testing in HTML validator
-.PHONY: validate val va v
-validate val va v: $(VALIDATE)
+#:article/art/a	article development
+.PHONY: article art a
+article art a: $(ARTICLE)
 
 #:all_cs/allcs/cs	creates files in ‘cs’ language
 .PHONY: all_cs allcs cs
-all_cs allcs cs: $(FOLDERS_cs) $(PREVIEW_cs) $(VALIDATE_cs) $(PUBLISH_cs) $(SPCHECK_cs)
+all_cs allcs cs: $(FOLDERS_cs) $(PREVIEW_cs) $(ARTICLE_cs) $(PUBLISH_cs) $(SPCHECK_cs)
 
 #:clean_cs/cl_cs/clcs/ccs	deletes ‘cs’ files
 .PHONY: clean_cs cl_cs clcs ccs
@@ -54,19 +54,21 @@ $(FOLDERS_cs):
 
 generovani-kodu-v-m4-uvod/%.html: rootb.m4 queues.m4 aux.m4 ent.m4 cfg.m4 inline.m4 headings.m4 block.m4 ver.m4 style.m4 lang_cs.m4 css.m4 refs_en.m4 refs_cs.m4 order.m4 lang.m4 incl.m4 $(wildcard intro.mc.html/*.m4 intro.mc.html/*/*.m4) %.m4 intro.mc nav.m4
 	m4 -DLANG_CODE='cs' -DOUTPUT_FILE='$*.html' $^ | sed -f brackets.sed > $@
-
-generovani-kodu-v-m4-uvod/publish.txt: rootb.m4 queues.m4 aux.m4 ent.m4 cfg.m4 inline.m4 headings.m4 block.m4 ver.m4 style.m4 lang_cs.m4 css.m4 refs_en.m4 refs_cs.m4 order.m4 lang.m4 incl.m4 $(wildcard intro.mc.html/*.m4 intro.mc.html/*/*.m4) publish.m4 intro.mc nav.m4
-	m4 -DLANG_CODE='cs' -DSOURCE='intro.mc' $^ | sed -f html/publish.sed -f brackets.sed > $@
+	@tidy -qe $@
 
 generovani-kodu-v-m4-uvod/spell.txt: rootb.m4 aux.m4 order.m4 lang.m4 headings.m4 ver.m4 lang_cs.m4 refs_en.m4 refs_cs.m4 incl.m4 spell.m4 intro.mc
 	m4 -DLANG_CODE='cs' -DSOURCE='intro.mc' $^ > $@
 
+generovani-kodu-v-m4-uvod/publish.txt: rootb.m4 queues.m4 aux.m4 ent.m4 cfg.m4 inline.m4 headings.m4 block.m4 ver.m4 style.m4 lang_cs.m4 css.m4 refs_en.m4 refs_cs.m4 order.m4 lang.m4 incl.m4 $(wildcard intro.mc.html/*.m4 intro.mc.html/*/*.m4) publish.m4 intro.mc nav.m4
+	m4 -DLANG_CODE='cs' -DSOURCE='intro.mc' $^ | sed -f html/publish.sed -f brackets.sed > $@
+
 generovani-kodu-v-m4-zaklady/%.html: rootb.m4 queues.m4 aux.m4 ent.m4 cfg.m4 inline.m4 headings.m4 block.m4 ver.m4 style.m4 lang_cs.m4 css.m4 refs_en.m4 refs_cs.m4 order.m4 lang.m4 incl.m4 $(wildcard fundamentals.mc.html/*.m4 fundamentals.mc.html/*/*.m4) %.m4 fundamentals.mc nav.m4
 	m4 -DLANG_CODE='cs' -DOUTPUT_FILE='$*.html' $^ | sed -f brackets.sed > $@
-
-generovani-kodu-v-m4-zaklady/publish.txt: rootb.m4 queues.m4 aux.m4 ent.m4 cfg.m4 inline.m4 headings.m4 block.m4 ver.m4 style.m4 lang_cs.m4 css.m4 refs_en.m4 refs_cs.m4 order.m4 lang.m4 incl.m4 $(wildcard fundamentals.mc.html/*.m4 fundamentals.mc.html/*/*.m4) publish.m4 fundamentals.mc nav.m4
-	m4 -DLANG_CODE='cs' -DSOURCE='fundamentals.mc' $^ | sed -f html/publish.sed -f brackets.sed > $@
+	@tidy -qe $@
 
 generovani-kodu-v-m4-zaklady/spell.txt: rootb.m4 aux.m4 order.m4 lang.m4 headings.m4 ver.m4 lang_cs.m4 refs_en.m4 refs_cs.m4 incl.m4 spell.m4 fundamentals.mc
 	m4 -DLANG_CODE='cs' -DSOURCE='fundamentals.mc' $^ > $@
+
+generovani-kodu-v-m4-zaklady/publish.txt: rootb.m4 queues.m4 aux.m4 ent.m4 cfg.m4 inline.m4 headings.m4 block.m4 ver.m4 style.m4 lang_cs.m4 css.m4 refs_en.m4 refs_cs.m4 order.m4 lang.m4 incl.m4 $(wildcard fundamentals.mc.html/*.m4 fundamentals.mc.html/*/*.m4) publish.m4 fundamentals.mc nav.m4
+	m4 -DLANG_CODE='cs' -DSOURCE='fundamentals.mc' $^ | sed -f html/publish.sed -f brackets.sed > $@
 
--- html_en.mk
+++ html_en.mk
@@ -6,22 +6,22 @@ FOLDERS_en = \
 generating-code-in-m4-introduction \
 generating-code-in-m4-fundamentals \
 
-PREVIEW_en  = $(FOLDERS_en:=/preview.html)
-VALIDATE_en = $(FOLDERS_en:=/validate.html)
-PUBLISH_en  = $(FOLDERS_en:=/publish.txt)
-SPCHECK_en  = $(FOLDERS_en:=/spell.txt)
+ARTICLE_en = $(FOLDERS_en:=/index.html)
+PREVIEW_en = $(FOLDERS_en:=/preview.html)
+PUBLISH_en = $(FOLDERS_en:=/publish.txt)
+SPCHECK_en = $(FOLDERS_en:=/spell.txt)
+ARTICLE  += $(FOLDERS_en) $(ARTICLE_en)
 FOLDERS  += $(FOLDERS_en)
 PREVIEW  += $(FOLDERS_en) $(PREVIEW_en)
-VALIDATE += $(FOLDERS_en) $(VALIDATE_en)
 PUBLISH  += $(FOLDERS_en) $(PUBLISH_en)
 SPCHECK  += $(FOLDERS_en) $(SPCHECK_en)
-TARGETS  += $(FOLDERS_en) $(PREVIEW_en) $(VALIDATE_en) $(PUBLISH_en) $(SPCHECK_en)
+TARGETS  += $(FOLDERS_en) $(PREVIEW_en) $(ARTICLE_en) $(PUBLISH_en) $(SPCHECK_en)
 
 #:html-sub-targets/sub/su	creates all files from generated rules
 .PHONY: html-sub-targets sub su
 html-sub-targets sub su: $(TARGETS)
 
-#:preview/pre/pr/p	for off-line article development
+#:preview/pre/pr/p	as close as possible in real website
 .PHONY: preview pre pr p
 preview pre pr p: $(PREVIEW)
 
@@ -33,13 +33,13 @@ spell sp: $(SPCHECK)
 .PHONY: publish pub pu
 publish pub pu: $(PUBLISH)
 
-#:validate/val/va/v	creates files for testing in HTML validator
-.PHONY: validate val va v
-validate val va v: $(VALIDATE)
+#:article/art/a	article development
+.PHONY: article art a
+article art a: $(ARTICLE)
 
 #:all_en/allen/en	creates files in ‘en’ language
 .PHONY: all_en allen en
-all_en allen en: $(FOLDERS_en) $(PREVIEW_en) $(VALIDATE_en) $(PUBLISH_en) $(SPCHECK_en)
+all_en allen en: $(FOLDERS_en) $(PREVIEW_en) $(ARTICLE_en) $(PUBLISH_en) $(SPCHECK_en)
 
 #:clean_en/cl_en/clen/cen	deletes ‘en’ files
 .PHONY: clean_en cl_en clen cen
@@ -54,19 +54,21 @@ $(FOLDERS_en):
 
 generating-code-in-m4-introduction/%.html: rootb.m4 queues.m4 aux.m4 ent.m4 cfg.m4 inline.m4 headings.m4 block.m4 ver.m4 style.m4 lang_en.m4 css.m4 refs_en.m4 refs_cs.m4 order.m4 lang.m4 incl.m4 $(wildcard intro.mc.html/*.m4 intro.mc.html/*/*.m4) %.m4 intro.mc nav.m4
 	m4 -DLANG_CODE='en' -DOUTPUT_FILE='$*.html' $^ | sed -f brackets.sed > $@
-
-generating-code-in-m4-introduction/publish.txt: rootb.m4 queues.m4 aux.m4 ent.m4 cfg.m4 inline.m4 headings.m4 block.m4 ver.m4 style.m4 lang_en.m4 css.m4 refs_en.m4 refs_cs.m4 order.m4 lang.m4 incl.m4 $(wildcard intro.mc.html/*.m4 intro.mc.html/*/*.m4) publish.m4 intro.mc nav.m4
-	m4 -DLANG_CODE='en' -DSOURCE='intro.mc' $^ | sed -f html/publish.sed -f brackets.sed > $@
+	@tidy -qe $@
 
 generating-code-in-m4-introduction/spell.txt: rootb.m4 aux.m4 order.m4 lang.m4 headings.m4 ver.m4 lang_en.m4 refs_en.m4 refs_cs.m4 incl.m4 spell.m4 intro.mc
 	m4 -DLANG_CODE='en' -DSOURCE='intro.mc' $^ > $@
 
+generating-code-in-m4-introduction/publish.txt: rootb.m4 queues.m4 aux.m4 ent.m4 cfg.m4 inline.m4 headings.m4 block.m4 ver.m4 style.m4 lang_en.m4 css.m4 refs_en.m4 refs_cs.m4 order.m4 lang.m4 incl.m4 $(wildcard intro.mc.html/*.m4 intro.mc.html/*/*.m4) publish.m4 intro.mc nav.m4
+	m4 -DLANG_CODE='en' -DSOURCE='intro.mc' $^ | sed -f html/publish.sed -f brackets.sed > $@
+
 generating-code-in-m4-fundamentals/%.html: rootb.m4 queues.m4 aux.m4 ent.m4 cfg.m4 inline.m4 headings.m4 block.m4 ver.m4 style.m4 lang_en.m4 css.m4 refs_en.m4 refs_cs.m4 order.m4 lang.m4 incl.m4 $(wildcard fundamentals.mc.html/*.m4 fundamentals.mc.html/*/*.m4) %.m4 fundamentals.mc nav.m4
 	m4 -DLANG_CODE='en' -DOUTPUT_FILE='$*.html' $^ | sed -f brackets.sed > $@
-
-generating-code-in-m4-fundamentals/publish.txt: rootb.m4 queues.m4 aux.m4 ent.m4 cfg.m4 inline.m4 headings.m4 block.m4 ver.m4 style.m4 lang_en.m4 css.m4 refs_en.m4 refs_cs.m4 order.m4 lang.m4 incl.m4 $(wildcard fundamentals.mc.html/*.m4 fundamentals.mc.html/*/*.m4) publish.m4 fundamentals.mc nav.m4
-	m4 -DLANG_CODE='en' -DSOURCE='fundamentals.mc' $^ | sed -f html/publish.sed -f brackets.sed > $@
+	@tidy -qe $@
 
 generating-code-in-m4-fundamentals/spell.txt: rootb.m4 aux.m4 order.m4 lang.m4 headings.m4 ver.m4 lang_en.m4 refs_en.m4 refs_cs.m4 incl.m4 spell.m4 fundamentals.mc
 	m4 -DLANG_CODE='en' -DSOURCE='fundamentals.mc' $^ > $@
+
+generating-code-in-m4-fundamentals/publish.txt: rootb.m4 queues.m4 aux.m4 ent.m4 cfg.m4 inline.m4 headings.m4 block.m4 ver.m4 style.m4 lang_en.m4 css.m4 refs_en.m4 refs_cs.m4 order.m4 lang.m4 incl.m4 $(wildcard fundamentals.mc.html/*.m4 fundamentals.mc.html/*/*.m4) publish.m4 fundamentals.mc nav.m4
+	m4 -DLANG_CODE='en' -DSOURCE='fundamentals.mc' $^ | sed -f html/publish.sed -f brackets.sed > $@
 
