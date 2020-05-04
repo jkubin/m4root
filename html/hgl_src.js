648d2d1a718085fd3533c2b5bb6613f9ebbd07b5
Josef Kubin <m4unix@gmail.com>
2020-05-04 23:55:01 +0200

    JavaScript decomposition

--- html/hgl_src.js
+++ html/hgl_src.js
@@ -318,48 +318,4 @@ init: function (all_keywords, all_sources,
 			}
 		}
 	}
-},
-/*
- * copy information from attributes to a dedicated element
- * this is important for the development of the articles
- * in order to copy&paste embedded data in one mouse click
- * made for standard browsers (not MSIE) that I use
- */
-add_info:function () {
-
-	var
-		parent_node = this.parentNode,
-		source_date,
-		source_info,
-		source_path;
-
-	if (this.source_info) {
-
-		if (this.source_info.isConnected)
-			parent_node.removeChild(this.source_info);
-		else
-			parent_node.appendChild(this.source_info);
-
-		return;
-	}
-
-	source_date = document.createElement("div");
-	source_info = document.createElement("div");
-	source_path = document.createElement("div");
-
-	// MSIE ignores it
-	if (window.getSelection)
-		source_date.onclick = source_path.onclick = function () {
-			const selected_text = window.getSelection();
-			const selected_range = document.createRange();
-			selected_range.selectNodeContents(this);
-			selected_text.removeAllRanges();
-			selected_text.addRange(selected_range);
-		};
-
-	source_date.appendChild(document.createTextNode(this.title));
-	source_path.appendChild(document.createTextNode(this.nextSibling.title.split('\n')[1]));
-	source_info.appendChild(source_date);
-	source_info.appendChild(source_path);
-	parent_node.appendChild(this.source_info = source_info);
 }
