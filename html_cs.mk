561cf55d53898b6424f091105271a858433a72e1
Josef Kubin <m4unix@gmail.com>
2020-05-05 00:03:00 +0200

    generated file

--- html_cs.mk
+++ html_cs.mk
@@ -17,11 +17,11 @@ PUBLISH  += $(FOLDERS_cs) $(PUBLISH_cs)
 SPCHECK  += $(FOLDERS_cs) $(SPCHECK_cs)
 TARGETS  += $(FOLDERS_cs) $(ARTICLE_cs) $(PREVIEW_cs) $(PUBLISH_cs) $(SPCHECK_cs)
 
-#:html-sub-targets/sub/su	creates all files from generated rules
+#:html-sub-targets/sub/su	creates all files from generated rules (default target)
 .PHONY: html-sub-targets sub su
 html-sub-targets sub su: $(TARGETS)
 
-#:preview/pre/pr/p	as close as possible in real website
+#:preview/pre/pr/p	creates html page as close as possible in real website
 .PHONY: preview pre pr p
 preview pre pr p: $(PREVIEW)
 
@@ -33,11 +33,11 @@ spell sp: $(SPCHECK)
 .PHONY: publish pub pu
 publish pub pu: $(PUBLISH)
 
-#:article/art/a	article development
+#:article/art/a	creates an article (this is a target for routine article development)
 .PHONY: article art a
 article art a: $(ARTICLE)
 
-#:all_cs/acs/cs	creates files in ‘cs’ language
+#:all_cs/acs/cs	creates ‘cs’ files
 .PHONY: all_cs acs cs
 all_cs acs cs: $(FOLDERS_cs) $(ARTICLE_cs) $(PREVIEW_cs) $(PUBLISH_cs) $(SPCHECK_cs)
 
@@ -46,29 +46,29 @@ all_cs acs cs: $(FOLDERS_cs) $(ARTICLE_cs) $(PREVIEW_cs) $(PUBLISH_cs) $(SPCHECK
 clean_cs cl_cs clcs ccs:
 	$(RM) -r $(FOLDERS_cs)
 
-#:fhtml	disabled for ‘cs’
+#:fhtml	is disabled for ‘cs’
 fhtml_cs.mk: ;
 
 $(FOLDERS_cs):
 	mkdir -p $@
 
 generovani-kodu-v-m4-uvod/%.html: $(JAVASCRIPT) rootb.m4 queues.m4 aux.m4 ent.m4 cfg.m4 inline.m4 headings.m4 block.m4 ver.m4 style.m4 lang_cs.m4 css.m4 js.m4 git.m4 refs_cs.m4 refs_en.m4 refs_mono.m4 order.m4 lang.m4 incl.m4 file.m4 cmd.m4 %.m4 intro.mc nav.m4
-	m4 -DLANG_CODE='cs' -DOUTPUT_FILE='$*.html' $(FLAGS)$(filter-out $(JAVASCRIPT), $^) | sed -f html/esc_to_ent.sed > $@
+	m4 -DLANG_CODE='cs' -DOUTPUT_FILE='$*.html' $(FLAGS) $(filter-out $(JAVASCRIPT), $^) | sed -f html/esc_to_ent.sed > $@
 	tidy -qe $@
 
 generovani-kodu-v-m4-uvod/spell.txt: rootb.m4 aux.m4 order.m4 lang.m4 headings.m4 ver.m4 lang_cs.m4 refs_cs.m4 refs_en.m4 refs_mono.m4 incl.m4 spell.m4 intro.mc
-	m4 -DLANG_CODE='cs' -DSOURCE='intro.mc' $(FLAGS)$^ > $@
+	m4 -DLANG_CODE='cs' -DSOURCE='intro.mc' $(FLAGS) $^ > $@
 
 generovani-kodu-v-m4-uvod/publish.txt: $(JAVASCRIPT) rootb.m4 queues.m4 aux.m4 ent.m4 cfg.m4 inline.m4 headings.m4 block.m4 ver.m4 style.m4 lang_cs.m4 css.m4 js.m4 git.m4 refs_cs.m4 refs_en.m4 refs_mono.m4 order.m4 lang.m4 incl.m4 file.m4 cmd.m4 publish.m4 intro.mc nav.m4
-	m4 -DLANG_CODE='cs' -DSOURCE='intro.mc' $(FLAGS)$(filter-out $(JAVASCRIPT), $^) | sed -f html/publish.sed -f html/esc_to_ent.sed > $@
+	m4 -DLANG_CODE='cs' -DSOURCE='intro.mc' $(FLAGS) $(filter-out $(JAVASCRIPT), $^) | sed -f html/publish.sed -f html/esc_to_ent.sed > $@
 
 generovani-kodu-v-m4-zaklady/%.html: $(JAVASCRIPT) rootb.m4 queues.m4 aux.m4 ent.m4 cfg.m4 inline.m4 headings.m4 block.m4 ver.m4 style.m4 lang_cs.m4 css.m4 js.m4 git.m4 refs_cs.m4 refs_en.m4 refs_mono.m4 order.m4 lang.m4 incl.m4 file.m4 cmd.m4 %.m4 fundamentals.mc nav.m4
-	m4 -DLANG_CODE='cs' -DOUTPUT_FILE='$*.html' $(FLAGS)$(filter-out $(JAVASCRIPT), $^) | sed -f html/esc_to_ent.sed > $@
+	m4 -DLANG_CODE='cs' -DOUTPUT_FILE='$*.html' $(FLAGS) $(filter-out $(JAVASCRIPT), $^) | sed -f html/esc_to_ent.sed > $@
 	tidy -qe $@
 
 generovani-kodu-v-m4-zaklady/spell.txt: rootb.m4 aux.m4 order.m4 lang.m4 headings.m4 ver.m4 lang_cs.m4 refs_cs.m4 refs_en.m4 refs_mono.m4 incl.m4 spell.m4 fundamentals.mc
-	m4 -DLANG_CODE='cs' -DSOURCE='fundamentals.mc' $(FLAGS)$^ > $@
+	m4 -DLANG_CODE='cs' -DSOURCE='fundamentals.mc' $(FLAGS) $^ > $@
 
 generovani-kodu-v-m4-zaklady/publish.txt: $(JAVASCRIPT) rootb.m4 queues.m4 aux.m4 ent.m4 cfg.m4 inline.m4 headings.m4 block.m4 ver.m4 style.m4 lang_cs.m4 css.m4 js.m4 git.m4 refs_cs.m4 refs_en.m4 refs_mono.m4 order.m4 lang.m4 incl.m4 file.m4 cmd.m4 publish.m4 fundamentals.mc nav.m4
-	m4 -DLANG_CODE='cs' -DSOURCE='fundamentals.mc' $(FLAGS)$(filter-out $(JAVASCRIPT), $^) | sed -f html/publish.sed -f html/esc_to_ent.sed > $@
+	m4 -DLANG_CODE='cs' -DSOURCE='fundamentals.mc' $(FLAGS) $(filter-out $(JAVASCRIPT), $^) | sed -f html/publish.sed -f html/esc_to_ent.sed > $@
 
--- html_en.mk
+++ html_en.mk
@@ -17,11 +17,11 @@ PUBLISH  += $(FOLDERS_en) $(PUBLISH_en)
 SPCHECK  += $(FOLDERS_en) $(SPCHECK_en)
 TARGETS  += $(FOLDERS_en) $(ARTICLE_en) $(PREVIEW_en) $(PUBLISH_en) $(SPCHECK_en)
 
-#:html-sub-targets/sub/su	creates all files from generated rules
+#:html-sub-targets/sub/su	creates all files from generated rules (default target)
 .PHONY: html-sub-targets sub su
 html-sub-targets sub su: $(TARGETS)
 
-#:preview/pre/pr/p	as close as possible in real website
+#:preview/pre/pr/p	creates html page as close as possible in real website
 .PHONY: preview pre pr p
 preview pre pr p: $(PREVIEW)
 
@@ -33,11 +33,11 @@ spell sp: $(SPCHECK)
 .PHONY: publish pub pu
 publish pub pu: $(PUBLISH)
 
-#:article/art/a	article development
+#:article/art/a	creates an article (this is a target for routine article development)
 .PHONY: article art a
 article art a: $(ARTICLE)
 
-#:all_en/aen/en	creates files in ‘en’ language
+#:all_en/aen/en	creates ‘en’ files
 .PHONY: all_en aen en
 all_en aen en: $(FOLDERS_en) $(ARTICLE_en) $(PREVIEW_en) $(PUBLISH_en) $(SPCHECK_en)
 
@@ -46,29 +46,29 @@ all_en aen en: $(FOLDERS_en) $(ARTICLE_en) $(PREVIEW_en) $(PUBLISH_en) $(SPCHECK
 clean_en cl_en clen cen:
 	$(RM) -r $(FOLDERS_en)
 
-#:fhtml	disabled for ‘en’
+#:fhtml	is disabled for ‘en’
 fhtml_en.mk: ;
 
 $(FOLDERS_en):
 	mkdir -p $@
 
 generating-code-in-m4-introduction/%.html: $(JAVASCRIPT) rootb.m4 queues.m4 aux.m4 ent.m4 cfg.m4 inline.m4 headings.m4 block.m4 ver.m4 style.m4 lang_en.m4 css.m4 js.m4 git.m4 refs_cs.m4 refs_en.m4 refs_mono.m4 order.m4 lang.m4 incl.m4 file.m4 cmd.m4 %.m4 intro.mc nav.m4
-	m4 -DLANG_CODE='en' -DOUTPUT_FILE='$*.html' $(FLAGS)$(filter-out $(JAVASCRIPT), $^) | sed -f html/esc_to_ent.sed > $@
+	m4 -DLANG_CODE='en' -DOUTPUT_FILE='$*.html' $(FLAGS) $(filter-out $(JAVASCRIPT), $^) | sed -f html/esc_to_ent.sed > $@
 	tidy -qe $@
 
 generating-code-in-m4-introduction/spell.txt: rootb.m4 aux.m4 order.m4 lang.m4 headings.m4 ver.m4 lang_en.m4 refs_cs.m4 refs_en.m4 refs_mono.m4 incl.m4 spell.m4 intro.mc
-	m4 -DLANG_CODE='en' -DSOURCE='intro.mc' $(FLAGS)$^ > $@
+	m4 -DLANG_CODE='en' -DSOURCE='intro.mc' $(FLAGS) $^ > $@
 
 generating-code-in-m4-introduction/publish.txt: $(JAVASCRIPT) rootb.m4 queues.m4 aux.m4 ent.m4 cfg.m4 inline.m4 headings.m4 block.m4 ver.m4 style.m4 lang_en.m4 css.m4 js.m4 git.m4 refs_cs.m4 refs_en.m4 refs_mono.m4 order.m4 lang.m4 incl.m4 file.m4 cmd.m4 publish.m4 intro.mc nav.m4
-	m4 -DLANG_CODE='en' -DSOURCE='intro.mc' $(FLAGS)$(filter-out $(JAVASCRIPT), $^) | sed -f html/publish.sed -f html/esc_to_ent.sed > $@
+	m4 -DLANG_CODE='en' -DSOURCE='intro.mc' $(FLAGS) $(filter-out $(JAVASCRIPT), $^) | sed -f html/publish.sed -f html/esc_to_ent.sed > $@
 
 generating-code-in-m4-fundamentals/%.html: $(JAVASCRIPT) rootb.m4 queues.m4 aux.m4 ent.m4 cfg.m4 inline.m4 headings.m4 block.m4 ver.m4 style.m4 lang_en.m4 css.m4 js.m4 git.m4 refs_cs.m4 refs_en.m4 refs_mono.m4 order.m4 lang.m4 incl.m4 file.m4 cmd.m4 %.m4 fundamentals.mc nav.m4
-	m4 -DLANG_CODE='en' -DOUTPUT_FILE='$*.html' $(FLAGS)$(filter-out $(JAVASCRIPT), $^) | sed -f html/esc_to_ent.sed > $@
+	m4 -DLANG_CODE='en' -DOUTPUT_FILE='$*.html' $(FLAGS) $(filter-out $(JAVASCRIPT), $^) | sed -f html/esc_to_ent.sed > $@
 	tidy -qe $@
 
 generating-code-in-m4-fundamentals/spell.txt: rootb.m4 aux.m4 order.m4 lang.m4 headings.m4 ver.m4 lang_en.m4 refs_cs.m4 refs_en.m4 refs_mono.m4 incl.m4 spell.m4 fundamentals.mc
-	m4 -DLANG_CODE='en' -DSOURCE='fundamentals.mc' $(FLAGS)$^ > $@
+	m4 -DLANG_CODE='en' -DSOURCE='fundamentals.mc' $(FLAGS) $^ > $@
 
 generating-code-in-m4-fundamentals/publish.txt: $(JAVASCRIPT) rootb.m4 queues.m4 aux.m4 ent.m4 cfg.m4 inline.m4 headings.m4 block.m4 ver.m4 style.m4 lang_en.m4 css.m4 js.m4 git.m4 refs_cs.m4 refs_en.m4 refs_mono.m4 order.m4 lang.m4 incl.m4 file.m4 cmd.m4 publish.m4 fundamentals.mc nav.m4
-	m4 -DLANG_CODE='en' -DSOURCE='fundamentals.mc' $(FLAGS)$(filter-out $(JAVASCRIPT), $^) | sed -f html/publish.sed -f html/esc_to_ent.sed > $@
+	m4 -DLANG_CODE='en' -DSOURCE='fundamentals.mc' $(FLAGS) $(filter-out $(JAVASCRIPT), $^) | sed -f html/publish.sed -f html/esc_to_ent.sed > $@
 
