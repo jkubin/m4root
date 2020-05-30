962f54fa025f1af9842c79bb31545f56b92d80a6
Josef Kubin <m4unix@gmail.com>
2020-05-30 19:26:05 +0200

    new records for files

--- git.sh
+++ git.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 # __HEADER([Josef Kubin], [2020/03/08], [m4root])
-# ___DESCR([extracts data from the git repository for M4 scripts])
-# ___POINT([fast code generation])
+# ___DESCR([generates a simple associative file database for the resulting generated code])
+# ___POINT([fast code generation using associative memory])
 
-echo '# DO NOT EDIT! This file is generated automatically!'
+cat << EOF
+# DO NOT EDIT! This file is generated automatically!
+#
+# This is a simple associative file database.
+#
+# define([./path/to/file], [[git commit hash], [git abbreviated commit hash], [git author date], [abbreviated sha1sum], [size [B]], [file type]])
+#
+EOF
 
 for filename in $@; do
-	git log -1 --pretty=format:"define([./$filename], [[%H], [%h], [%ai]])%n" -- $filename
+	git log -1 --date='format:%Y%m%d.%T' --pretty="format:define([./$filename], [[%H], [%h], [%ad], [" -- $filename
+	sha1sum $filename | head -c 7
+	stat --printf="], [%s], [%F]])\n" $filename
 done
